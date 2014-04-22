3601a2995dcab5a063bbc7ef2d3da61546ead9fa
igoryen <igor.yentaltsev@gmail.com>
2014-04-22 18:55:03 -0400

    InitializeIdentityForEF(): added

--- igoryen2/Models/Initializer.cs
+++ igoryen2/Models/Initializer.cs
@@ -7,5 +7,8 @@ using System.Data.Entity;
 
 namespace igoryen2.Models {
     public class Initializer : DropCreateDatabaseAlways<DataContext> {
+
+        private void InitializeIdentityForEF(DataContext dc) { 
+        }
     }
 }
