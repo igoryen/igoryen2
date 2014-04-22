508b1465ed7abf3f0d8a0bd667db85227190cb05
igoryen <igor.yentaltsev@gmail.com>
2014-04-22 17:46:44 -0400

    Initial commit

---
+++ igoryen2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(igoryen2.Startup))]
+namespace igoryen2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
