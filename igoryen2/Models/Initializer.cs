760f346676399566f5c1cff465a58eb13e832e5d
igoryen <igor.yentaltsev@gmail.com>
2014-04-22 18:58:22 -0400

    IdentityRole: three added

--- igoryen2/Models/Initializer.cs
+++ igoryen2/Models/Initializer.cs
@@ -14,6 +14,21 @@ namespace igoryen2.Models {
 
             var UserManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(dc));
             var RoleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(dc));
+
+            string roleName1 = "Admin";
+            if (!RoleManager.RoleExists(roleName1)) {
+                var role1CreateResult = RoleManager.Create(new IdentityRole(roleName1));
+            }
+
+            string roleName2 = "Student";
+            if (!RoleManager.RoleExists(roleName2)) {
+                var role2CreateResult = RoleManager.Create(new IdentityRole(roleName2));
+            }
+
+            string roleName3 = "Faculty";
+            if (!RoleManager.RoleExists(roleName3)) {
+                var role3CreateResult = RoleManager.Create(new IdentityRole(roleName3));
+            }
         }
     }
 }
