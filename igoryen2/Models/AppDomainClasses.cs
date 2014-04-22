f93bb18f7e3a7238e6a96d1b830056ab7b8c729a
igoryen <igor.yentaltsev@gmail.com>
2014-04-22 18:22:28 -0400

    Models/AppdomainClasses.cs: added

---
+++ igoryen2/Models/AppDomainClasses.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace igoryen2.Models {
+
+    public class Cancellation {
+        public int CancellationId { get; set; }
+        public Faculty Faculty { get; set; }
+        public Course Course { get; set; }
+        public string Date { get; set; }
+        public string Message { get; set; }
+    }
+
+    public class ComMethod {
+        public int ComMethodId { get; set; }
+        public string Handle { get; set; }
+        public string CellNo { get; set; }
+        public string Email { get; set; }
+    }
+
+    public class Course { // 10
+        public Course() {
+            this.Faculty = new Faculty();
+            this.Students = new List<Student>();
+        }
+        [Key]
+        public int CourseId { get; set; }
+        [Required]
+        public string CourseCode { get; set; }
+        [Required]
+        public string CourseName { get; set; }
+        public string RoomNumber { get; set; }
+        public string RunTime { get; set; }
+        public Faculty Faculty { get; set; }
+        public List<Student> Students { get; set; }
+        public virtual ApplicationUser User { get; set; }
+
+    }
+
+    public class Faculty : Person {
+        public Faculty() {
+            this.Courses = new List<Course>();
+            SenecaId = string.Empty;
+        }
+
+        public Faculty(string fname, string lname, string phone, string senId)
+            : base(fname, lname, phone) {
+            this.Courses = new List<Course>();
+            SenecaId = senId;
+        }
+
+        [Required]
+        [RegularExpression("^[0][0-9]{8}$", ErrorMessage = "0 followed by 8 digits")]
+        public string SenecaId { get; set; }
+        public List<Course> Courses { get; set; }
+    }
+
+    public class Person : ApplicationUser { // 10
+
+        //public class Person : IdentityUser { // 10
+
+
+        public Person() {
+            FirstName = LastName = Phone = string.Empty;
+        }
+
+        public Person(string f, string l, string p) {
+            FirstName = f;
+            LastName = l;
+            Phone = p;
+        }
+
+        [Key]
+        public int PersonId { get; set; }
+
+        [Required]
+        [StringLength(40, MinimumLength = 3)]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [StringLength(100, MinimumLength = 3)]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+        [Required]
+        [RegularExpression("^[2-9]\\d{2}-\\d{3}-\\d{4}$", ErrorMessage = "nnn-nnn-nnnn")]
+        public string Phone { get; set; }
+    }
+
+    public class Student : Person {
+        public Student() {
+            SenecaId = string.Empty;
+            this.Courses = new List<Course>();
+        }
+
+        public Student(string f, string l, string p, string senId)
+            : base(f, l, p) {
+            SenecaId = senId;
+        }
+
+        [Required]
+        [RegularExpression("^[0][0-9]{8}$", ErrorMessage = "0 followed by 8 digits")]
+        public string SenecaId { get; set; }
+        public List<Course> Courses { get; set; }
+        //public List<ComMethod> ComMethods { get; set; }
+    }
+
+}
--- igoryen2/Models/IdentityModels.cs
+++ igoryen2/Models/IdentityModels.cs
@@ -1,17 +1,15 @@
 ﻿using Microsoft.AspNet.Identity.EntityFramework;
 
-namespace igoryen2.Models
-{
+namespace igoryen2.Models {
     // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit http://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
-    public class ApplicationUser : IdentityUser
-    {
+    public class ApplicationUser : IdentityUser {
+        public string HomeTown { get; set; }
+        public virtual MyUserInfo MyUserInfo { get; set; }
     }
 
-    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
-    {
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser> {
         public ApplicationDbContext()
-            : base("DefaultConnection")
-        {
+            : base("DefaultConnection") {
         }
     }
 }
