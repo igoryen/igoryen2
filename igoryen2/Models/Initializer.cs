edfe57a4aaf2caaf219ec434c2c87f9990c7deeb
igoryen <igor.yentaltsev@gmail.com>
2014-04-22 18:56:28 -0400

    UserManager & RoleManager: added

--- igoryen2/Models/Initializer.cs
+++ igoryen2/Models/Initializer.cs
@@ -3,12 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Data.Entity;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
 
 
 namespace igoryen2.Models {
     public class Initializer : DropCreateDatabaseAlways<DataContext> {
 
-        private void InitializeIdentityForEF(DataContext dc) { 
+        private void InitializeIdentityForEF(DataContext dc) {
+
+            var UserManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(dc));
+            var RoleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(dc));
         }
     }
 }
