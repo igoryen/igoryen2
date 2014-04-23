ddd38f3ad930916c2c0b91396c5246d7590c4479
igoryen <igor.yentaltsev@gmail.com>
2014-04-23 16:35:15 -0400

    ViewModels/VM_Cancellation.cs/CancellationCreateForHttpGet: created

--- igoryen2/ViewModels/VM_Cancellation.cs
+++ igoryen2/ViewModels/VM_Cancellation.cs
@@ -2,7 +2,23 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
+
 
 namespace igoryen2.ViewModels {
+
+    public class CancellationCreateForHttpGet {
+        [Key]
+        public int CancellationId { get; set; }
+        public SelectList CourseSelectList { get; set; }
+        [Display(Name = "Date :)")]
+        public string Date { get; set; }
+        public string Message { get; set; }
+        public void Clear() {
+            Date = string.Empty;
+            Message = string.Empty;
+        }
+    }
 
 }
