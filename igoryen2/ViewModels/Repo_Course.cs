dc45d8e721bc96e6aab37ffd1d6f708a51baf50e
igoryen <igor.yentaltsev@gmail.com>
2014-04-23 16:04:51 -0400

    Repo_Course.cs/getListOfCourseBaseAM(): added

--- igoryen2/ViewModels/Repo_Course.cs
+++ igoryen2/ViewModels/Repo_Course.cs
@@ -7,6 +7,12 @@ using System.Web.Mvc;
 namespace igoryen2.ViewModels {
     public class Repo_Course : RepositoryBase {
 
+        public IEnumerable<CourseBase> getListOfCourseBaseAM(string currentUserId) {
+            //var courses = dc.Courses.Where(course => course.User.Id == currentUserId).ToList();
+            if (courses == null) return null;
+            return Mapper.Map<IEnumerable<CourseBase>>(courses);
+        }
+
         public SelectList getSelectListOfCourse(string currentUserId) {
             IEnumerable<CourseBase> cbs = getListOfCourseBaseAM(currentUserId);
             SelectList sl = new SelectList(cbs, "CourseId", "CourseCode");
