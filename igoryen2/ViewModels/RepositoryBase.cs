e120e7ece63129899e48ca6a942bfdf7d98e5962
igoryen <igor.yentaltsev@gmail.com>
2014-04-23 15:26:21 -0400

    ViewModels/RepositoryBase.cs: finished v1

--- igoryen2/ViewModels/RepositoryBase.cs
+++ igoryen2/ViewModels/RepositoryBase.cs
@@ -2,8 +2,19 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using igoryen2.Models;
 
 namespace igoryen2.ViewModels {
     public class RepositoryBase {
+
+        protected DataContext dc;
+
+        public RepositoryBase() {
+
+            dc = new DataContext();
+
+            dc.Configuration.ProxyCreationEnabled = false;
+            dc.Configuration.LazyLoadingEnabled = false;
+        }
     }
 }
