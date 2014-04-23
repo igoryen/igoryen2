47a427ce96b769b3e73cb9dd6e77470a55dc5e5d
igoryen <igor.yentaltsev@gmail.com>
2014-04-23 16:00:32 -0400

    VM/getSelectListOfCourse(): added

--- igoryen2/ViewModels/Repo_Course.cs
+++ igoryen2/ViewModels/Repo_Course.cs
@@ -2,8 +2,16 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
 
 namespace igoryen2.ViewModels {
-    public class Repo_Course {
+    public class Repo_Course : RepositoryBase {
+
+        public SelectList getSelectListOfCourse(string currentUserId) {
+            IEnumerable<CourseBase> cbs = getListOfCourseBaseAM(currentUserId);
+            SelectList sl = new SelectList(cbs, "CourseId", "CourseCode");
+            return sl;
+        }
+
     }
 }
