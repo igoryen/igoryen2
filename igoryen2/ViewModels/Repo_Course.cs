0bd3267b8750d468383f8a251f47d3af80b9d27d
igoryen <igor.yentaltsev@gmail.com>
2014-04-23 16:18:17 -0400

    Repo_Course.cs/getListOfCourseBase(): made

--- igoryen2/ViewModels/Repo_Course.cs
+++ igoryen2/ViewModels/Repo_Course.cs
@@ -7,14 +7,23 @@ using System.Web.Mvc;
 namespace igoryen2.ViewModels {
     public class Repo_Course : RepositoryBase {
 
-        public IEnumerable<CourseBase> getListOfCourseBaseAM(string currentUserId) {
-            //var courses = dc.Courses.Where(course => course.User.Id == currentUserId).ToList();
+        public IEnumerable<CourseBase> getListOfCourseBase(string currentUserId) {
+            var courses = dc.Courses.Where(course => course.Students.Any(st => st.Id == currentUserId)).ToList();
             if (courses == null) return null;
-            return Mapper.Map<IEnumerable<CourseBase>>(courses);
+            List<CourseBase> lcb = new List<CourseBase>();
+            foreach (var item in courses) {
+                CourseBase cb = new CourseBase();
+                cb.CourseCode = item.CourseCode;
+                cb.CourseId = item.CourseId;
+                cb.CourseName = item.CourseName;
+                lcb.Add(cb);
+            }
+
+            return lcb.ToList();
         }
 
         public SelectList getSelectListOfCourse(string currentUserId) {
-            IEnumerable<CourseBase> cbs = getListOfCourseBaseAM(currentUserId);
+            IEnumerable<CourseBase> cbs = getListOfCourseBase(currentUserId);
             SelectList sl = new SelectList(cbs, "CourseId", "CourseCode");
             return sl;
         }
