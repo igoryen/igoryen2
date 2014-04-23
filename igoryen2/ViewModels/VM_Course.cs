c21becb160b462c809fc22129f41db684d6073d3
igoryen <igor.yentaltsev@gmail.com>
2014-04-23 16:08:31 -0400

    VM_Course.cs/CourseBase: added

--- igoryen2/ViewModels/VM_Course.cs
+++ igoryen2/ViewModels/VM_Course.cs
@@ -2,7 +2,16 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
+
 
 namespace igoryen2.ViewModels {
+
+    public class CourseBase {
+        [Key]
+        public int CourseId { get; set; }
+        public string CourseCode { get; set; }
+        public string CourseName { get; set; }
+    }
 
 }
