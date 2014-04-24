cacdbd589e4a28664a118960487ce7093d7d91b3
igoryen <igor.yentaltsev@gmail.com>
2014-04-24 11:59:24 -0400

    Repo_Cancellation.cs/buildCancellation(): v1

--- igoryen2/ViewModels/Repo_Cancellation.cs
+++ igoryen2/ViewModels/Repo_Cancellation.cs
@@ -2,9 +2,25 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using igoryen2.Models;
+
 
 namespace igoryen2.ViewModels {
     public class Repo_Cancellation {
+        private DataContext db = new DataContext();
+
+
+        // v1
+        public Cancellation buildCancellation(CancellationCreateForHttpPost newItem, ApplicationUser currentUser) {
+            Cancellation cancellation = new Cancellation();
+            cancellation.CancellationId = newItem.CancellationId;
+            cancellation.Course = db.Courses.Find(newItem.CourseId);
+            cancellation.Date = newItem.Date;
+            cancellation.Faculty = db.Faculties.FirstOrDefault(f => f.Id == currentUser.Id);
+            cancellation.Message = newItem.Message;
+
+            return cancellation;
+        }
 
     }
 }
