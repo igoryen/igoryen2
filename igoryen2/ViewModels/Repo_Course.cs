0cd11c550d107c37e453bf66dfc949f01a3eb24e
igoryen <igor.yentaltsev@gmail.com>
2014-04-28 16:02:10 -0400

    getCourseFull() v1.

--- igoryen2/ViewModels/Repo_Course.cs
+++ igoryen2/ViewModels/Repo_Course.cs
@@ -7,6 +7,9 @@ using System.Web.Mvc;
 namespace igoryen2.ViewModels {
     public class Repo_Course : RepositoryBase {
 
+        private Repo_Faculty rf = new Repo_Faculty();
+        private Repo_Student rs = new Repo_Student();
+
         public IEnumerable<CourseBase> getListOfCourseBase(string currentUserId) {
             var courses = dc.Courses.Where(course => course.Faculty.Id == currentUserId).ToList();
             if (courses == null) return null;
@@ -33,6 +36,27 @@ namespace igoryen2.ViewModels {
             }
             SelectList sl = new SelectList(lcb.ToList(), "CourseId", "CourseCode");
             return sl;
+        }
+
+        //v1
+        public CourseFull getCourseFull(int? CourseId) {
+            if (CourseId == null) return null;
+            var course = dc.Courses.Include("Students").Include("Faculty").SingleOrDefault(c => c.CourseId == CourseId);
+            if (course == null) return null;
+            CourseFull cf = new CourseFull();
+            cf.CourseCode = course.CourseCode;
+            cf.CourseId = course.CourseId;
+            cf.CourseName = course.CourseName;
+            cf.DateEnd = course.DateEnd;
+            cf.DateStart = course.DateStart;
+            cf.Faculty = rf.getFacultyFull(course.Faculty.PersonId);
+            cf.RoomNo = course.RoomNumber;
+            List<StudentFull> lsf = new List<StudentFull>();
+            foreach(var item in course.Students){
+                lsf.Add(rs.getStudentFull(item.PersonId));
+            }
+            cf.Students = lsf;
+            return cf;
         }
 
     }
